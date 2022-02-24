7197a471bad2b8c85a33f5b77c7bead15b7b7540
Madi <mkuanyshbek97@mail.ru>
2022-02-24 20:50:10 +0600

    builds synced, app.properties added

---
+++ app/src/main/java/com/example/materialdesigntest/utils/EquilateralImageView.kt
@@ -0,0 +1,9 @@
+package com.example.materialdesigntest.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class EquilateralImageView (context: Context, attributeSet: AttributeSet ? = null, defStyle: Int = 0) : AppCompatImageView
+    (context, attributeSet, defStyle) {
+}
